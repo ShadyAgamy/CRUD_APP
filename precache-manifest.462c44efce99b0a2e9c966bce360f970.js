7c6f66d7e2afa05958ae6649d8223a126f2c146c
Shady Agamy <shadyalonsoo@gmail.com>
2022-03-10 14:04:05 +0200

    Updates

---
+++ precache-manifest.462c44efce99b0a2e9c966bce360f970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e9846b927b4461ca276",
+    "url": "/CRUD_APP/static/css/main.51ba0d10.chunk.css"
+  },
+  {
+    "revision": "9e9846b927b4461ca276",
+    "url": "/CRUD_APP/static/js/main.9e9846b9.chunk.js"
+  },
+  {
+    "revision": "abf4f13b884d789f1b03",
+    "url": "/CRUD_APP/static/css/1.9dde4fe6.chunk.css"
+  },
+  {
+    "revision": "abf4f13b884d789f1b03",
+    "url": "/CRUD_APP/static/js/1.abf4f13b.chunk.js"
+  },
+  {
+    "revision": "88118d46878b728b3f9f",
+    "url": "/CRUD_APP/static/js/runtime~main.88118d46.js"
+  },
+  {
+    "revision": "751de303e064e9191bbfe0d4c0cb6b08",
+    "url": "/CRUD_APP/index.html"
+  }
+];
